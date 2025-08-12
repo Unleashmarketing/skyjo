714f5bb0c90cd456a071a1af7520bfffe9ed06e0
Unleashmarketing <unleashjonas@gmail.com>
2025-08-12 13:19:40 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SkyjoApp from './SkyjoApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SkyjoApp />
+  </React.StrictMode>
+)
